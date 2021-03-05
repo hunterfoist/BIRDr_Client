ee538476c18b82c9bdf1553f78705b7ba07437a5
Hunter Foist <hfoist@gmail.com>
2021-03-04 19:29:59 -0500

    fixed image cards

--- src/components/BirdLog.jsx
+++ src/components/BirdLog.jsx
@@ -62,55 +62,84 @@ const useStyles = makeStyles({
     },
   });
   const classes = useStyles();
-  const birdMapper = () => {
-    return props.birds.map((bird, index) => {
-      return(
-        <div>
-          <Card className={classes.root}>
+  // const birdMapper = () => {
+  //   // return props.birds.map((bird, index) => {
+  //     return(
+  //       <div>
+  //         <Card className={classes.root}>
+  //     <CardActionArea>
+  //       <CardMedia
+  //         className={classes.media}
+  //         image={bird.image_id}
+  //         title="Contemplative Reptile"
+  //       />
+  //       <CardContent>
+  //         <Typography align='center' gutterBottom variant="h5" component="h2">
+  //         {bird.species}
+  //         </Typography>
+  //         <Typography align='center' variant="body2" color="textSecondary" component="p">
+  //         {bird.location}
+  //         </Typography>
+  //         <Typography align='center' variant="body2" color="textSecondary" component="p">
+  //         {bird.date} || {bird.time}
+  //         </Typography>
+  //         <Typography align='center' variant="body2" color="textSecondary" component="p">
+  //         {bird.rarity}
+  //         </Typography>
+  //         <Typography align='center' variant="body2" color="textSecondary" component="p">
+  //         {bird.secret}
+  //         </Typography>
+  //       </CardContent>
+  //     </CardActionArea>
+  //     <CardActions>
+  //     <Button  color="warning" onClick={() => {props.editUpdateBird(bird); handleClickOpen(); props.updateOn()}} >Update</Button>
+  //     <Button color="danger" onClick={() => deleteBird(bird)}>Delete</Button>
+  //     </CardActions>
+  //   </Card>
+        
+  //       </div>
+  //     );
+  //   // });
+  // };
+  
+  return(
+    <>
+      
+    
+      {/* <Table alignItem="baseline">
+        <tbody> */}
+        <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image={bird.image_id}
+          image={props.bird.image_id}
           title="Contemplative Reptile"
         />
         <CardContent>
           <Typography align='center' gutterBottom variant="h5" component="h2">
-          {bird.species}
+          {props.bird.species}
           </Typography>
           <Typography align='center' variant="body2" color="textSecondary" component="p">
-          {bird.location}
+          {props.bird.location}
           </Typography>
           <Typography align='center' variant="body2" color="textSecondary" component="p">
-          {bird.date} || {bird.time}
+          {props.bird.date} || {props.bird.time}
           </Typography>
           <Typography align='center' variant="body2" color="textSecondary" component="p">
-          {bird.rarity}
+          {props.bird.rarity}
           </Typography>
           <Typography align='center' variant="body2" color="textSecondary" component="p">
-          {bird.secret}
+          {props.bird.secret}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-      <Button  color="warning" onClick={() => {props.editUpdateBird(bird); handleClickOpen(); props.updateOn()}} >Update</Button>
-      <Button color="danger" onClick={() => deleteBird(bird)}>Delete</Button>
+      <Button  color="warning" onClick={() => {props.editUpdateBird(props.bird); handleClickOpen(); props.updateOn()}} >Update</Button>
+      <Button color="danger" onClick={() => deleteBird(props.bird)}>Delete</Button>
       </CardActions>
     </Card>
-        
-        </div>
-      );
-    });
-  };
-  
-  return(
-    <>
-      <h3>Bird History</h3>
-      
-      <Table alignItem="baseline">
-        <tbody>
-          {birdMapper()}
-        </tbody>
-      </Table>
+        {/* </tbody>
+      </Table> */}
     </>
   );
 };
--- src/components/BirdNest.jsx
+++ src/components/BirdNest.jsx
@@ -46,6 +46,10 @@ const BirdNest = props => {
 
   useEffect(() => fetchBirds(), [])
 
+  function displayCards() {
+    return birds.length > 0 ? birds.map((bird) => <BirdLog bird={bird} birds={birds} editUpdateBird={editUpdateBird} updateOn={updateOn} fetchBirds={fetchBirds} token={props.token} />) : null;
+}
+
 
   return(
     <Container flexDirection="row" alignItems="flex-start">
@@ -54,7 +58,8 @@ const BirdNest = props => {
           <BirdCreate fetchBirds={fetchBirds} token={props.token} />
         </Grid>
         <Grid container item xs="9" alignItems="flex-start">
-        <BirdLog birds={birds} editUpdateBird={editUpdateBird} updateOn={updateOn} fetchBirds={fetchBirds} token={props.token} /> 
+          {displayCards()}
+        {/* <BirdLog birds={birds} editUpdateBird={editUpdateBird} updateOn={updateOn} fetchBirds={fetchBirds} token={props.token} />  */}
           {updateActive ? <BirdEdit birdToUpdate={birdToUpdate} updateOn={updateOn} updateOff={updateOff} token={props.token} fetchBirds={fetchBirds}/> : <></>}
         </Grid>
         </Grid>
