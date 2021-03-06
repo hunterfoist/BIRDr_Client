e4fbf7709b6d125b33a451eee02d7082920e4696
Eva Walls <evawalls@evas-mbp.lan>
2021-03-06 12:44:52 -0500

    sync evawalls

--- src/components/BirdCreate.jsx
+++ src/components/BirdCreate.jsx
@@ -9,6 +9,41 @@ import { FormControlLabel } from '@material-ui/core';
 import Checkbox from '@material-ui/core/Checkbox'
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
+import { ReactComponent as BirdrIcon } from "../assets/birdrrbracket.svg";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  palette: {
+    primary: {
+      main: '#aecbea',
+    },
+    secondary: {
+      main: '#c2b092',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  BirdrIcon: {
+    margin: theme.spacing(1),
+    backgroundColor: '#c2b092',
+  },
+  form: {
+    width: '100%', 
+    marginTop: theme.spacing(1),
+  },
+  button: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: '#eae3cb',
+  },
+  checkbox: {
+    backgroundColor: '#b65f50',
+  },
+}));
 
 const BirdCreate = props => {
   const [ species, setSpecies ] = useState('');
@@ -66,9 +101,16 @@ const BirdCreate = props => {
 
   return(
     <>
+    
+
+    
+  
+    <Container alignItem="baseline">
+    <br/>
+    <br/>
+    <br/>
     <h3>Create an Entry!</h3>
-    <Container>
-      
+    <br/>
     <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
@@ -135,7 +177,7 @@ const BirdCreate = props => {
                 autoComplete="time"
               />
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={12}>Rarity
             <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
@@ -147,7 +189,7 @@ const BirdCreate = props => {
                 value={rarity}
                 label="Rarity"
                 id="rarity"
-                autoComplete="rarity"
+                autoComplete="Rarity"
                 >
                   <MenuItem value={1}>Common</MenuItem>
                   <MenuItem value={2}>Uncommon</MenuItem>
@@ -183,7 +225,7 @@ const BirdCreate = props => {
                 label="Would you like this entry to be private?"
                 id="secret"
                 autoComplete="secret"
-                control={<Checkbox value="Yes" color="primary" />}
+                control={<Checkbox value="Yes"/>}
           />  
             </Grid>
             
@@ -193,10 +235,9 @@ const BirdCreate = props => {
             onClick={handleSubmit}
             fullWidth
             variant="contained"
-            color="primary"
           >
             Create Log!
-          </Button>
+          </Button >
     </Container>
     </>
   );
